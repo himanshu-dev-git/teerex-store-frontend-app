8c473f65a4e340c2161c33938999ce291461e10e
Himanshu Ranjan <himanshuimpdoc@gmail.com>
2023-02-01 15:34:26 +0530

    updated

---
+++ src/utils/CustomHooks.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export const useWindowWidth = () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [timerId, setTimerId] = useState(null);
+
+    useEffect(() => {
+        const debounceResizeHandler = () => {
+            if(timerId) {
+                clearTimeout(timerId);
+            }
+
+            let newTimer = setTimeout(() => {
+                setWidth(window.innerWidth)
+            }, 100);
+
+            setTimerId(newTimer);
+        }
+
+        window.addEventListener('resize', debounceResizeHandler)
+
+        return () => window.removeEventListener('resize', debounceResizeHandler);
+    }, [])
+
+    return width;
+    
+}
+
